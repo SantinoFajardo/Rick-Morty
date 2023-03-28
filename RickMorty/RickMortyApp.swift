d1545d8fdd67f120a8db68f5e852722f8c644ff4
FajardoSatino <santino@sphereone.xyz>
2023-03-28 20:05:24 -0300

    Initial Commit

---
+++ RickMorty/RickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApp.swift
+//  RickMorty
+//
+//  Created by Santino Fajardo on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
